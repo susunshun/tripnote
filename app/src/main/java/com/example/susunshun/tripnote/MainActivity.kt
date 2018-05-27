a0f885a5da0a1b53796285bae6319bb290b61815
Shun Ono <susunshun@gmail.com>
2018-05-28 01:17:18 +0900

    Floating Action Buttonを追加

--- app/src/main/java/com/example/susunshun/tripnote/MainActivity.kt
+++ app/src/main/java/com/example/susunshun/tripnote/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.example.susunshun.tripnote
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), RecyclerViewHolder.ItemClickListener {
 
+class MainActivity : AppCompatActivity(), RecyclerViewHolder.ItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +16,10 @@ class MainActivity : AppCompatActivity(), RecyclerViewHolder.ItemClickListener {
         val hoges = listOf("aaa", "bbb", "CCC").toMutableList()
         mainRecyclerView.adapter = RecyclerAdapter(this, this, hoges)
         mainRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        add.setOnClickListener {
+            Toast.makeText(applicationContext, "Hello, Snackbar!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onItemClick(view: View, position: Int) {
